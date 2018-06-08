e84052438de830504de2e0ae2f502b9c5b57c450
Avielyo10 <avielyo10@gmail.com>
2018-06-08 11:20:53 +0300

    beautify the final print

--- TestCase.cpp
+++ TestCase.cpp
@@ -6,6 +6,14 @@ TestCase::TestCase(string name, ostream &out){
 }
 
 TestCase TestCase::print(){
-       *op <<testName<<": "<<failed<<" failed, "<<passed<<" passed, "<<failed+passed<<" total."<<endl<<"\n===============================================================================================\n\n"; 
+       string d,s = testName+": "+to_string(failed)+" failed, "+to_string(passed)+" passed, "+to_string(failed+passed)+" total.#";
+       int counter = s.size()+1;
+       while(counter!=0){
+               d+="#";
+               counter--;
+       }
+       *op <<"\n"+d;
+       *op <<"\n#"+s<<endl; 
+       *op <<d+"\n=================================================================================================\n\n";
        return *this;
 }
